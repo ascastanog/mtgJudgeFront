91b8dbab346cca281b397666e5f8149593b47d00
Alvaro <alvaroscasgar@gmail.com>
2022-06-06 21:59:22 +0200

    casi casi

--- judgeAppFront/src/app/app.component.ts
+++ judgeAppFront/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
+import {filter, Subscription} from "rxjs";
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,67 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'judgeAppFront';
+
+  public subscriber: Subscription | undefined;
+
+  constructor(private router: Router, @Inject(DOCUMENT) private document: any){ }
+
+  ngOnInit(): void {
+    this.subscriber = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      console.log('The URL changed to: ' + event['url'])
+      if(event['url']==='/login'){
+        this.document.body.classList.remove('bgverde');
+        this.document.body.classList.remove('bgblanco');
+        this.document.body.classList.remove('bgnegro');
+        this.document.body.classList.remove('bgazul');
+        this.document.body.classList.remove('bgrojo');
+        this.document.body.classList.add('bgblanco');
+      }else if(event['url']==='/home-players'){
+        this.document.body.classList.remove('bgverde');
+        this.document.body.classList.remove('bgblanco');
+        this.document.body.classList.remove('bgnegro');
+        this.document.body.classList.remove('bgazul');
+        this.document.body.classList.remove('bgrojo');
+        this.document.body.classList.add('bgnegro');
+      }else if(event['url']==='/nuevo-deck'){
+        this.document.body.classList.remove('bgverde');
+        this.document.body.classList.remove('bgblanco');
+        this.document.body.classList.remove('bgnegro');
+        this.document.body.classList.remove('bgazul');
+        this.document.body.classList.remove('bgrojo');
+        this.document.body.classList.add('bgazul');
+      }else if(event['url']==='/tusTorneos'){
+        this.document.body.classList.remove('bgverde');
+        this.document.body.classList.remove('bgblanco');
+        this.document.body.classList.remove('bgnegro');
+        this.document.body.classList.remove('bgazul');
+        this.document.body.classList.remove('bgrojo');
+        this.document.body.classList.add('bgrojo');
+      }else if(event['url']==='/torneo'){
+        this.document.body.classList.remove('bgverde');
+        this.document.body.classList.remove('bgblanco');
+        this.document.body.classList.remove('bgnegro');
+        this.document.body.classList.remove('bgazul');
+        this.document.body.classList.remove('bgrojo');
+        this.document.body.classList.add('bgverde');
+      }
+    /*  else{
+        this.document.body.classList.remove('bg1');
+        this.document.body.classList.add('bg2');
+      }
+
+      /nuevo-deck
+      /tusTorneos
+      /torneo
+*/
+    });
+  }
+
+  ngOnDestroy () {
+    this.subscriber?.unsubscribe();
+  }
+
+
 }
