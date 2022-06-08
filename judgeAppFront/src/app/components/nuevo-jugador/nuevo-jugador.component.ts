202c46b33e1b2e3dab58b3b890c170f31efc55a3
Alvaro <alvaroscasgar@gmail.com>
2022-06-08 23:05:52 +0200

    terminado

--- judgeAppFront/src/app/components/nuevo-jugador/nuevo-jugador.component.ts
+++ judgeAppFront/src/app/components/nuevo-jugador/nuevo-jugador.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../../service/auth.service";
 import {NuevoJugador} from "../../models/nuevo-jugador";
 import {Router} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-nuevo-jugador',
@@ -30,22 +31,22 @@ export class NuevoJugadorComponent implements OnInit {
 
 
 
-  constructor(private authService: AuthService, private router:Router) {
+  constructor(private authService: AuthService, private router:Router, private toastr : ToastrService) {
   }
 
   ngOnInit(): void {
   }
 
   crearJugador() {
-    console.log("peticion para crear usuario");
+
 
     this.nuevoJugador = new NuevoJugador(this.nombre, this.nombreUsuario, this.email, this.DCI, this.password, this.authorities=['PLAYER'])
     console.log(this.nuevoJugador)
     this.authService.nuevo(this.nuevoJugador).subscribe(
       data =>{
-
+      this.toastr.success("Bienvenido")
       }, err =>{
-       console.log("error en el crear usuario")
+      this.toastr.error("No ha sido posible crear el usuario")
     }
     )
     this.router.navigate(['login'])
